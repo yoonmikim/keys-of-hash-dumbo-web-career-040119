ffecf9abf59a2d2f3d49150679faba9fa0ab2a3d
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-26 23:21:48 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,27 @@
+# class Hash
+#   def keys_of(arguments)
+#     # code goes here
+#     arr = []
+#     map { |k, v| 
+#       if arguments == v 
+#         arr << k
+#       end
+#     }
+#     arr
+#   end
+# end
+
 class Hash
   def keys_of(arguments)
     # code goes here
+    arr = []
+    map { |k, v| 
+      if arguments == v 
+        arr.push(k)
+      else if arguments.include?(v)
+        arr.push(k)
+      end
+    }
+    arr
   end
 end
