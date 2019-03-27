4bea09e5e583404de4ac2ca98f75af44c274e5a9
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-27 01:55:31 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -11,17 +11,50 @@
 #   end
 # end
 
+# class Hash
+#   def keys_of(arguments)
+#     # code goes here
+#     arr = []
+#     map do |k, v| 
+#       arr.push(k) if arguments == v 
+#     end
+#     arr
+#   end
+# end
+
+# class Hash
+#   def keys_of(*args)
+#     map {|key, value| args.include?(value) ? key : nil }.compact
+#   end
+# end
+
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*args)
+    arr = []
+    map do |key, value|
+      if args.include?(value)
+        arr << key
+      end
+    end
+
+  return arr
+  end
+end
+
+
+class Hash
+  def keys_of(*arguments)
     # code goes here
     arr = []
-    map { |k, v| 
-      if arguments == v 
-        arr.push(k)
-      else if arguments.include?(v)
-        arr.push(k)
+    
+    arguments.each do |args|
+      self.each do |key,value|
+        if value == args
+          arr << key
+        end
       end
-    }
-    arr
+    end
+  return arr
   end
 end
