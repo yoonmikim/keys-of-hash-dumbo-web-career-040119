2de4ac7800e483b5e780478a665882001c614960
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-27 01:24:58 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -27,3 +27,9 @@
 #     map {|key, value| args.include?(value) ? key : nil }.compact
 #   end
 # end
+
+class Hash
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
+  end
+end
