7ffc29c50547f0ef9723ead5eb39344ed9d4c11c
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-27 01:55:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -41,3 +41,20 @@ class Hash
   return arr
   end
 end
+
+
+class Hash
+  def keys_of(*arguments)
+    # code goes here
+    arr = []
+    
+    arguments.each do |args|
+      self.each do |key,value|
+        if value == args
+          arr << key
+        end
+      end
+    end
+  return arr
+  end
+end
